ee6ed51000700e027047c37f097ff28bcba7e8d8
Zhengyang Liu <liuz@cs.utah.edu>
2021-09-27 10:20:51 -0700

    add a few semantics back

--- tools/transform.cpp
+++ tools/transform.cpp
@@ -779,7 +779,7 @@ static uint64_t aligned_alloc_size(uint64_t size, unsigned align) {
   return add_saturate(size, align - 1);
 }
 
-static void calculateAndInitConstants(Transform &t) {
+void calculateAndInitConstants(Transform &t) {
   if (!bits_program_pointer)
     initBitsProgramPointer(t);
 
