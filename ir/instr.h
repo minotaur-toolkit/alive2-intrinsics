e0ca8646ba35d375b0a44aaf616d1dd474ee1720
Zhengyang Liu <liuz@cs.utah.edu>
2022-03-10 20:56:28 -0800

    pack*sdw

--- ir/instr.h
+++ ir/instr.h
@@ -1135,7 +1135,7 @@ public:
 
 class X86IntrinBinOp final : public Instr {
 public:
-  static constexpr unsigned numOfX86Intrinsics = 126;
+  static constexpr unsigned numOfX86Intrinsics = 132;
   enum Op {
 #define PROCESS(NAME,A,B,C,D,E,F) NAME,
 #include "intrinsics.h"
