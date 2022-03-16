2106bd0aafd746b5a8c5f04f1904acfa8199b1bf
Zhengyang Liu <liuz@cs.utah.edu>
2022-03-15 23:36:44 -0700

    dont alow approx for synth

--- llvm_util/llvm2alive.cpp
+++ llvm_util/llvm2alive.cpp
@@ -1135,7 +1135,7 @@ public:
     default:
       break;
     }
-    return visitCallInst(i, true);
+    return error(i); //visitCallInst(i, true);
   }
 
   RetTy visitExtractElementInst(llvm::ExtractElementInst &i) {
